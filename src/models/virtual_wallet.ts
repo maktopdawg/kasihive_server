7aa867130594f7af6cefc9cb23e96fb6b845a382
bradley <bradleyranaka@gmail.com>
2024-10-04 18:59:03 +0000

    Virtual wallet schema

---
+++ src/models/virtual_wallet.ts
@@ -0,0 +1,32 @@
+import mongoose, { Schema, model } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const VirtualWalletSchema = new Schema({
+    investor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Investor',
+        required: true
+    },
+    business: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Business',
+        required: true
+    },
+    investmentRequest: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'InvestmentRequest',
+        required: true
+    },
+    initialInvestment: {
+        type: Number,
+        required: true
+    },
+    currentBalance: {
+        type: Number,
+        default: 0
+    }
+});
+
+export default model('VirtualAccount', VirtualWalletSchema)
