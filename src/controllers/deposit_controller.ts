70d41732fe836ee93e833f61d133c32b1e068a32
Jessicaaa <99355359+Jessica-Chuene@users.noreply.github.com>
2024-10-05 22:20:44 +0000

    Fixed Props and deposit routes

--- src/controllers/deposit_controller.ts
+++ src/controllers/deposit_controller.ts
@@ -1,55 +1,51 @@
+// deposit_controller.ts
 import { Request, Response } from "express";
 import deposit from "../models/deposit";
 
-interface DepositController{
-    investorId : string
-    amount: number
-    dateDeposited: Date
-
+interface DepositProps {
+    investorId: string;
+    amount: number;
+    dateDeposited: Date;
 }
 
-
 class DepositController {
+    static create_deposit = async (req: Request, res: Response) => {
+        const { investorId, amount, dateDeposited }: DepositProps = req.body;
 
-    static create_deposit_account = async (req: Request, res: Response) => {
-        const { investorId, amount, dateDeposited }: DepositController = req.body;
-    
         if (!investorId || !amount || !dateDeposited) {
-            return res.status(200).json({message: "All fields are required."})
+            return res.status(400).json({ message: "All fields are required." });
         }
 
         try {
             const result = await deposit.create({
-                "investorId": investorId,
-                "amount": amount,
-                "dateDeposited": dateDeposited
-            })
-            res.status(201).json({ message: "Deposit successfully created"})
+                investorId,
+                amount,
+                dateDeposited,
+            });
+            res.status(201).json({ message: "Deposit successfully created", deposit: result });
         } catch (error: any) {
-            res.status(500).json({ "error": error.message, "message": "Error creating deposit"});
+            res.status(500).json({ error: error.message, message: "Error creating deposit" });
         }
-    }
+    };
 
     static get_deposit = async (req: Request, res: Response) => {
         const { id } = req.params;
 
-        if ( !id ){
-            return res.status(400).json({message: "Deposit id is required."})
+        if (!id) {
+            return res.status(400).json({ message: "Deposit id is required." });
         }
         try {
             const result = await deposit.findById(id);
 
             if (!result) {
-                return res.status(404).json({ message: "Deposit record is not found"})
+                return res.status(404).json({ message: "Deposit record not found" });
             }
 
-            res.status(200).json(result)
-
-        } catch(error:any) {
-            res.status(500).json({ error: error.message, message: 'Error retrieving performance record'});
+            res.status(200).json(result);
+        } catch (error: any) {
+            res.status(500).json({ error: error.message, message: "Error retrieving deposit record" });
         }
-    }
-
-} 
+    };
+}
 
 export default DepositController;
