d9f7406918a48bb403940d5cac425e3e9895e449
bradley <bradleyranaka@gmail.com>
2024-10-05 21:33:11 +0000

    performanceRoutes added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 // Call all the routes here
 import { Router } from "express";
 import accountsRouter from "./accounts"
+import performanceRouter from "./performanceRoutes"
 
 const router: Router = Router();
 
 router.use( accountsRouter )
+router.use( performanceRouter )
+
 
 export default router;
