b3beb52732cd01d710fda4ed85c3b7c1f7b47a3e
maktopdawg <142145714+maktopdawg@users.noreply.github.com>
2024-10-05 21:21:38 +0000

    added stuff

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,14 @@
 import { Router } from "express";
 import accountsRouter from "./accounts"
 import investmentRequestRouter from "./investment_request"
+import virtualWalletRouter from "./virtual_wallet"
+import InvestmentRouter from "./investment"
 
 const router: Router = Router();
 
 router.use( accountsRouter )
 router.use( investmentRequestRouter )
+router.use( virtualWalletRouter )
+router.use( InvestmentRouter )
 
 export default router;
--- src/routes/investment_request.ts
+++ src/routes/investment_request.ts
@@ -3,6 +3,10 @@ import InvestmentsRequestController from "../controllers/investment_request_cont
 
 const router: Router = Router();
 
-router.post( "/dashboard/:id/create-new-investment-request", InvestmentsRequestController.open_investment_request )
+router.post( "/dashboard/:id/new-investment-request", InvestmentsRequestController.open_investment_request )
+router.get( "/investment-requests/all", InvestmentsRequestController.get_all_investment_request )
+router.get( "/investment-requests/:id", InvestmentsRequestController.get_investment_request )
+router.post( "/investment-requests/add-investor", InvestmentsRequestController.add_investor_to_investment_request )
+router.post( "/investment-requests/remove-investor", InvestmentsRequestController.remove_investor_from_investment_request )
 
 export default router;
